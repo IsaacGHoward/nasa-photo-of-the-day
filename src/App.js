30caea504d39f54c6c560d5f6c6fa8b838174a35
IsaacGHoward <howardisaac100@gmail.com>
2020-10-20 17:18:27 -0700

    Finally got proper date including the local timezone, and formatted it for the get request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-
+const axios = require('axios').default;
+//API Key: idq4GBavwG9WN9OQgxeyodtdTdmD96I8jYaOb6Hu
 function App() {
+  const apiKey = 'idq4GBavwG9WN9OQgxeyodtdTdmD96I8jYaOb6Hu';
+  const [APOD_data, setAPOD] = useState();
+  const today = new Date().toLocaleDateString();
+  const formattedDay = today.slice(6)+'-'+today.slice(0,2)+'-'+today.slice(3,5);
+  console.log(formattedDay);
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${formattedDay}`)
+    .then(res => console.log(res));
+  }, []);
   return (
     <div className="App">
       <p>
