0738171f3df3b330d7a70776874bb7bb5a2bada7
IsaacGHoward <howardisaac100@gmail.com>
2020-10-22 13:43:37 -0700

    Added reactstrap, toggler that toggles info about the picture of the day

--- src/components/details.js
+++ src/components/details.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { UncontrolledCollapse, Button, CardBody, Card } from 'reactstrap';
 
 function Details(props) {
+  //Copyright : {props.data.copyright ? props.data.copyright : 'N/A'}
     return (
-    <div>
-        <p>
-          {props.data.explanation}
+    <div className="details">
+        <p style={{margin: 'auto', marginTop: '.5rem', marginBottom: '.5rem'}}>
+          {'Title: ' + props.data.title}
         </p>
-        <p>
-          Copyright : {props.data.copyright} | Title : {props.data.title}
-        </p>
+        <Button color="primary" id="toggler" style={{ marginBottom: '.5rem' }}>More info</Button>
+        <UncontrolledCollapse toggler="#toggler">
+          <p>
+            Explanation:  {props.data.explanation} | Copyright : {props.data.copyright ? props.data.copyright : 'N/A'}
+          </p>
+        </UncontrolledCollapse>
     </div>
     );
   }
